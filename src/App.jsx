b0bcc70d568a7a5d9d71282ace95132bb9e8f75e
Joywei2003 <133044119+Joywei2003@users.noreply.github.com>
2025-06-18 01:03:18 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import './App.css'
+import { flashcards } from './data.js';
+import { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(1)
+  const [side, setSide] = useState('type')
+  const totalCards = flashcards.length;
+  const cardIndex = count - 1;
+  const currentFlashcard = flashcards[cardIndex];
+  
+  const nextFlashcard = () => {
+    setCount(count === totalCards ? 1 : count + 1);
+    setSide('type');
+  }
+  const lastFlashcard = () => {
+    setCount(count === 1 ? totalCards : count - 1);
+    setSide('type');
+  }
+  const switchSide = () => {
+    setSide(side === 'type' ? 'image' : 'type');
+  }
+
+  return (
+    <div className='App'>
+      <div className='header'>
+        <div className='title'>Types of cats</div>
+        <div className='instructions'>
+          Click the card to filp it. Click the arrouws to move between the cards.
+        </div>
+        <div className='cardNumber'>Card: {count}/{totalCards}</div>
+      </div>
+      <div className='flashCard' onClick={switchSide}>
+        {side === 'type' ? (
+          <div className='flashCard-type'>{currentFlashcard.type}</div>
+        ) : (
+          <div className='flashCard-image'>
+            <img className='catImage' src={currentFlashcard.image} alt={currentFlashcard.type} />
+          </div>
+        )}
+      </div>
+      <div className='buttons'>
+        <button onClick={lastFlashcard}> &larr; </button>
+        <button onClick={nextFlashcard}> &rarr; </button>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/data.js
@@ -0,0 +1,53 @@
+import britishShorthairImage from "./images/britishShorthair.jpeg"
+import SiameseImage from "./images/Siamese.jpeg"
+import SiberianImage from "./images/Siberian.jpeg"
+import ScottishFoldImage from "./images/scottishFold.jpeg"
+import AmericanBobtailImage from "./images/americanBobtail.jpeg"
+import BritishLonghairImage from "./images/britishLonghair.jpeg"
+import TurkishVanImage from "./images/turkishVan.jpeg"
+import RagdollImage from "./images/Ragdoll.jpeg"
+import ChartreuxImage from "./images/chartreux.jpeg"
+import JapaneseBobtailImage from "./images/japaneseBobtail.jpeg"
+
+export const flashcards = [
+    {
+        type: 'British Shorthair',
+        image: britishShorthairImage
+    },
+    {
+        type: 'Siamese',
+        image: SiameseImage
+    },
+    {
+        type: 'Siberian',
+        image: SiberianImage
+    },
+    {
+        type: 'Scottish Fold',
+        image: ScottishFoldImage
+    },
+    {
+        type: 'American Bobtail',
+        image: AmericanBobtailImage
+    },
+    {
+        type: 'British Longhair',
+        image: BritishLonghairImage
+    },
+    {
+        type: 'Turkish Van',
+        image: TurkishVanImage
+    },
+    {
+        type: 'Ragdoll',
+        image: RagdollImage
+    },
+    {
+        type: 'Chartreux',
+        image: ChartreuxImage
+    },
+    {
+        type: 'Japanese Bobtail',
+        image: JapaneseBobtailImage
+    }
+];
